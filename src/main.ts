b99dd8b7728ff24e643c8fb52937c6f26b69ae38
Jordão Qualho <jordaoqualho@gmail.com>
2023-07-29 17:23:37 -0300

    chore: adding some packages and setting main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.enableCors();
   await app.listen(3000);
+  Logger.debug(`Listening on http://localhost:${3000}/`);
 }
 bootstrap();
