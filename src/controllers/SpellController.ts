015c0076dd31cf7caab861a28beb5bc9965c589c
Jordão Qualho <jordaoqualho@gmail.com>
2023-12-01 16:45:15 -0300

    feat: 🎸 creating spell controller
    ✅ Closes: DA-16

---
+++ src/controllers/SpellController.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { SpellDto } from 'src/dtos';
+import { Public } from 'src/providers';
+import { SpellService } from 'src/services';
+
+@Controller('spell')
+export class SpellController {
+  constructor(private readonly spellService: SpellService) {}
+
+  @Public()
+  @Post()
+  async createSpell(@Body() payload: SpellDto) {
+    return await this.spellService.createSpell(payload);
+  }
+
+  @Public()
+  @Get('/:spellId?')
+  async findSpell(
+    @Param('spellId') spellId?: string,
+  ): Promise<SpellDto | SpellDto[]> {
+    if (spellId) return await this.spellService.findSpellById(spellId);
+    return await this.spellService.findAllSpell();
+  }
+
+  @Public()
+  @Put('/:spellId')
+  async updateSpell(
+    @Param('spellId') spellId: string,
+    @Body() updateSpellData: SpellDto,
+  ): Promise<SpellDto> {
+    return this.spellService.updateSpell(spellId, updateSpellData);
+  }
+
+  @Public()
+  @Delete('/:spellId')
+  async deleteSpell(@Param('spellId') spellId: string): Promise<void> {
+    return this.spellService.deleteSpell(spellId);
+  }
+
+  @Delete()
+  async deleteAllSpells(): Promise<void> {
+    return this.spellService.deleteAllSpells();
+  }
+}
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,4 @@
 export * from './AuthController';
 export * from './CharacterController';
-
+export * from './SpellController';
 export * from './UserController';
